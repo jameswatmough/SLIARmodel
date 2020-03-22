52fa2370d4bd77b4d33135f2edd2b747dca6a563
James Watmough <watmough@unb.ca>
2020-03-22 10:56:19 -0300

    wraped a few more little scripts into functions

--- SLIAR-SSA.R
+++ SLIAR-SSA.R
@@ -1,4 +1,4 @@
-# First, set up a Gillespie simulation of the SLIAR model 
+
 # Second, set up the ode solver for the same model with the same parameters
 
 # load the libraries
@@ -20,7 +20,7 @@ simName <- "SLIAR epidemic model"
 # the model consists of specifying 
 # the changes in the state variables (transitions) 
 # and the rates for each event
-rates <- c("beta*S*(epsilon*L + I + delta*A)",    
+rates <- c("beta*S*(epsilon*L + I + delta*A)/(S+L+I+A+R)",    
            "p*kappa*L",
            "(1-p)*kappa*L",
            "eta*A",
@@ -43,17 +43,50 @@ nu  <- matrix(c(-1,  0, 0,   0,  0,  0,
 # for missing parameters allows use of global variables
 # note the switch to log variables to avoid numerical problems near zero populations
 SLIARrhs <- function(t,x, parms=NULL) {
-  with(as.list(c(x,parms)),{
+	with(as.list(c(x,parms)),{
 
-    dS = -beta*S*(epsilon*L + I + delta*A);
-    dL = beta*S*(epsilon*L + I + delta*A) - kappa*L;
-    dI = p*kappa*L - alpha*I;
-    dA = (1-p)*kappa*L - eta*A;
-    dR = f*alpha*I + eta*A;
-    list(c(dS,dL,dI,dA,dR))
+		dS = -beta*S*(epsilon*L + I + delta*A)/(S+L+I+A+R);
+		dL = beta*S*(epsilon*L + I + delta*A)/(S+L+I+A+R) - kappa*L;
+		dI = p*kappa*L - alpha*I;
+		dA = (1-p)*kappa*L - eta*A;
+		dR = f*alpha*I + eta*A;
+		list(c(dS,dL,dI,dA,dR))
 
-  })
+	})
 
 }
+
+
+Ro = function(param) {
+	with(param, beta*(epsilon/kappa + p/alpha + (1-p)*delta/eta))
+}
+
+
+# run 20 simulations and store the results in a big matrix
+# set verbose FALSE to suppress all the output from ssa
+
+repsim = function(S0,rates,nu,param, tf, simName, runs, method = ssa.d()) {
+	A = data.frame(NULL)
+	for (run in seq(1,runs)) {
+	res_ssa <- ssa(x0 = S0,
+		a=rates,nu,parms = param,tf,
+		method=method,simName,
+		verbose=FALSE)
+	# ssa.otl for taul leaping, dt for 
+# output of ssa includes $data, which has t, S, L, ... for each event
+# append a fourth column with run number to ssa$data
+# bind result to A (add it as additional rows to bottom)
+		A = rbind(A,data.frame(res_ssa$data,run=as.factor(run)))
+	}
+	return(A)
+}
+
+pluckrun = function(data, sim, col = 3:5) {
+	wide = subset(data, run==sim)
+	long = stack(wide[,col])
+	long$t = rep(wide$t,times=length(col))
+	return(long)
+}
+
 
 
---
+++ SLIAR-testruns.R
@@ -0,0 +1,56 @@
+# Run GillespieSSA with the SLIAR model and compare with the solution from the ODE
+# 
+
+library("ggplot2")
+
+source("SLIAR-SSA.R")
+
+
+# read in parameter values based on various scenarios
+
+paramset <- read.csv("parameter-sets.csv")
+
+
+Ro = with(paramset, beta*(epsilon/kappa + p/alpha + (1-p)*delta/eta) )
+
+# use the second parameter set  (Diamond Princess paper)
+param = paramset[2,2:9]
+
+# Initial state vector for ssa
+# A = 1 for the initial 'invasion'
+# I = 20, A = 40 for the 'switch on distancing' scenario
+S0  <- c(S=1000, L=0, I = 1, A = 0, R = 0)               
+#S0  <- c(S=1000, L=0, I = 20, A = 40, R = 0)               
+
+# final time for SSA
+tf <- 60  
+# output times for ode solver
+times <- seq(0, tf, by=0.01)
+
+# 20 simulations 
+A = repsim(S0,rates,nu,param=param[2,2:9],tf=360,simName,runs=20)
+
+# plot R for every sim
+ggplot(A,aes(x=t,y=R))+geom_line(aes(color = run))
+
+# plot L, I, A for one sim
+wide = subset(A,run==6)
+long = stack(wide[,3:5])
+long$t = rep(wide$t,times=3)
+ggplot(long,aes(x=t,y=values))+geom_line(aes(color = ind))
+
+# run the ode simulator 
+# one trick to get numerical solutions of population models without problems
+# from round-off errors introducint negative populations is to transform the model to log
+# populations,
+# This means the initial conditions for the ode need to be log transformed
+# initial state vector for ode (log)
+#x0 <- log(S0)
+#names(x0) = c("S","L","I","A","R")
+res_ode <- ode(y = S0, times, func = SLIARrhs, parms)
+
+# need to figure out how to use ggplot to add the ode results in
+# for now just make a new plot
+dev.new()
+plot(res_ode[,1],res_ode[,'I'],col='red')
+
